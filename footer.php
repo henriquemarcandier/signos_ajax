53cf84d3abaf8c98cbbcaf48e9401982daaaa590
Henrique Marcandier <henrique.marcandier@gmail.com>
2024-08-28 22:37:36 -0300

    Add files via upload

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 &copy; <?=date('Y')?> - Desenvolvido pela <a href='https://www.bhcommerce.com.br' target='_blank'>BH Commerce</a>
 </footer>
 <hr>
-<script src="https://www.bhcommerce.com.br/plugins/jquery/jquery.min.js"></script>
+<script src="./assets/js/jquery.min.js"></script>
 <script src="./assets/js/scripts.js"></script>
 </body>
 </html>
